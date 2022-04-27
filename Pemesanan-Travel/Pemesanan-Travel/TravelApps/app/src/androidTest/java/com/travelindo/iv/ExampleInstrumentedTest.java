453caa74d8046dd5f4e6fa9e004894ea59bb791f
raflihadi2 <raflihadi2@gmail.com>
2022-04-27 23:44:01 +0700

    UTS Pemrograman Mobile

---
+++ Pemesanan-Travel/Pemesanan-Travel/TravelApps/app/src/androidTest/java/com/travelindo/iv/ExampleInstrumentedTest.java
@@ -0,0 +1,27 @@
+package com.travelindo.iv;
+
+import static org.junit.Assert.assertEquals;
+
+import android.content.Context;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnitRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnitRunner.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.travelindo", appContext.getPackageName());
+    }
+}
